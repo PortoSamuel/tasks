939a9046fe1d17978fd6a79634f2110a539642f1
PortoSamuel <38635893+PortoSamuel@users.noreply.github.com>
2021-11-26 11:15:34 -0300

    apt bd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
-        'HOST': '127.0.0.1',
+        'HOST': '18.221.139.211',
         'PORT': '5432',
     }
 }
