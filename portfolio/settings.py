e2f4c71e7b225404756c6dcb23dc6f9302fb4703
PortoSamuel <38635893+PortoSamuel@users.noreply.github.com>
2021-11-26 11:06:08 -0300

    update db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
-        'HOST': '127.0.0.1/32',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
